e7d65ba3303e24ad2c9b79d575d96004466f31f0
Odilon Alves <odilon.neto@cabal.com.br>
2020-04-01 16:09:20 -0300

    WIP

--- policies/example/0.1/example.lua
+++ policies/example/0.1/example.lua
@@ -1,12 +1,12 @@
-local setmetatable = setmetatable
-
 local _M = require('apicast.policy').new('Example', '0.1')
-local mt = { __index = _M }
-
-local cjson = require('cjson.safe')
+local cjson = require('cjson')
 local http_ng = require 'resty.http_ng'
 
-function _M.new()
+local new = _M.new
+
+function _M.new(config)
+  local self = new(config)
+  
   self.config = config or {}
   self.http_client = http_ng.new{
     backend = config.client
@@ -14,22 +14,10 @@ function _M.new()
   
   ngx.log(ngx.INFO, 'example policy new')
 
-  return setmetatable({}, mt)
+  return self
 end
 
-function _M:init()
-  -- do work when nginx master process starts
-end
-
-function _M:init_worker()
-  -- do work when nginx worker process is forked from master
-end
-
-function _M:rewrite()
-  -- change the request before it reaches upstream
-end
-
-function _M:access()
+function _M:access(context)
   -- ability to deny the request before it is sent upstream
   local res, err = self.http_client.post{'https://sippe-acl.requestcatcher.com/test' , { data = 'sent from 3scale policy'}, headers = {['Authorization'] = 'admin:admin'}}
   
@@ -40,31 +28,6 @@ function _M:access()
   ngx.log(ngx.INFO, 'example policy access')
 
   return true
-end
-
-function _M:content()
-  -- can create content instead of connecting to upstream
-end
-
-function _M:post_action()
-  -- do something after the response was sent to the client
-end
-
-function _M:header_filter()
-  -- can change response headers
-end
-
-function _M:body_filter()
-  -- can read and change response body
-  -- https://github.com/openresty/lua-nginx-module/blob/master/README.markdown#body_filter_by_lua
-end
-
-function _M:log()
-  -- can do extra logging
-end
-
-function _M:balancer()
-  -- use for example require('resty.balancer.round_robin').call to do load balancing
 end
 
 return _M
