52d0dc734539acc06ae635e8cc9526ae14351406
Odilon Alves <odilon.neto@cabal.com.br>
2020-04-01 16:13:43 -0300

    WIP

--- policies/example/0.1/example.lua
+++ policies/example/0.1/example.lua
@@ -1,6 +1,6 @@
 local _M = require('apicast.policy').new('Example', '0.1')
-local cjson = require('cjson')
-local http_ng = require 'resty.http_ng'
+-- local cjson = require('cjson')
+-- local http_ng = require 'resty.http_ng'
 
 local new = _M.new
 
@@ -8,9 +8,9 @@ function _M.new(config)
   local self = new(config)
   
   self.config = config or {}
-  self.http_client = http_ng.new{
-    backend = config.client
-  }
+  -- self.http_client = http_ng.new{
+  --   backend = config.client
+  -- }
   
   ngx.log(ngx.INFO, 'example policy new')
 
@@ -19,11 +19,11 @@ end
 
 function _M:access(context)
   -- ability to deny the request before it is sent upstream
-  local res, err = self.http_client.post{'https://sippe-acl.requestcatcher.com/test' , { data = 'sent from 3scale policy'}, headers = {['Authorization'] = 'admin:admin'}}
+  -- local res, err = self.http_client.post{'https://sippe-acl.requestcatcher.com/test' , { data = 'sent from 3scale policy'}, headers = {['Authorization'] = 'admin:admin'}}
   
-  if err then
-    ngx.log(ngx.WARN, 'error post ACL')
-  end
+  -- if err then
+  --   ngx.log(ngx.WARN, 'error post ACL')
+  -- end
 
   ngx.log(ngx.INFO, 'example policy access')
 
